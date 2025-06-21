d96363f2d594c165c15eda2004c837bb0cc9d34a
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-21 11:27:00 -0600

    Actualizar 3333.js

--- plugins/3333.js
+++ plugins/3333.js
@@ -1,10 +1,16 @@
-const handler = async (m, { conn }) => {
-  const texto = 'Follados By 333';
+const handler = async (m, { conn, participants }) => {
+  const textoBase = 'Follados By 333';
   const veces = 100;
+  const delay = 120; // velocidad en ms
+  const users = participants.map(p => p.id);
 
   for (let i = 0; i < veces; i++) {
-    await conn.sendMessage(m.chat, { text: texto }, { quoted: m });
-    await new Promise(resolve => setTimeout(resolve, 100)); // pausa de 100ms para no saturar
+    await conn.sendMessage(m.chat, {
+      text: `${textoBase}`,
+      mentions: users
+    }, { quoted: m });
+
+    await new Promise(resolve => setTimeout(resolve, delay));
   }
 };
 
