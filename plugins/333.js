1585eac63f92a3bb0cccfcabb3c7f03e0bd24179
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-20 19:05:35 -0600

    Actualizar 333.js

--- plugins/333.js
+++ plugins/333.js
@@ -1,17 +1,17 @@
-const handler = async (m, { conn, text }) => {
+const handler = async (m, { conn }) => {
   if (m.text?.toLowerCase().trim() !== '333') return;
 
-  const img = 'https://qu.ax/GbxoW.jpg';
   const mensaje = `
 𝐏𝐮𝐫𝐨 𝐏𝐢𝐧𝐜𝐡𝐞 𝟑𝟑𝟑 𝐀𝐥𝐯
 𝐄𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐬𝐞𝐫𝐚 𝐑𝐨𝐛𝐚𝐝𝐨 𝐁𝐲 𝟑𝟑𝟑
 𝐘 𝐪𝐮𝐞 𝐡𝐚𝐫𝐚𝐧 𝐢𝐧𝐮𝐭𝐢𝐥𝐞𝐬 𝐧𝐚𝐝𝐚
 `.trim();
 
-  await conn.sendFile(m.chat, img, '333.jpg', mensaje, m);
+  const imgBuffer = await (await fetch('https://qu.ax/GbxoW.jpg')).buffer();
+  await conn.sendMessage(m.chat, { image: imgBuffer, caption: mensaje }, { quoted: m });
 };
 
 handler.customPrefix = /^333$/i;
-handler.command = new RegExp; // sin comando, solo texto exacto
+handler.command = new RegExp; // sin comando
 
 export default handler;
